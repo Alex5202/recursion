f2b961d243d24479ca0c689ccb42cde72816ddb8
s7emen <alsae@yandex.ru>
2024-03-11 21:20:26 +0700

    Добавлен файл решение задания 1 task1

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+class Program
+{
+    static void Main()
+    {
+        // Задайте значения M и N
+        int M = 2;
+        int N = 11;
+
+        // Вызов метода для вывода натуральных чисел в заданном промежутке
+        PrintNumbersInRange(M, N);
+
+        Console.ReadLine(); // Для того чтобы консольное окно не закрылось сразу
+    }
+
+    // Рекурсивный метод для вывода натуральных чисел в заданном промежутке
+    static void PrintNumbersInRange(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNumbersInRange(start + 1, end);
+        }
+    }
+}
+
